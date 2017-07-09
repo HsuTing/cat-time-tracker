ef5610a664a44023f953d22ad1e4dace763d28d2
HsuTing <hsuting0106@gmail.com>
2017-07-09 15:18:48 +0800

    add count today

--- src/countTime.js
+++ src/countTime.js
@@ -16,19 +16,26 @@ const outputTime = time => {
 
 export default () => {
   const tagsTotal = {...tags};
+  const today = moment();
+  let todayTotal = 0;
 
   Object.keys(tags).forEach(tag => {
     tagsTotal[tag] = 0;
   });
 
   const total = Store.time.reduce((nowTotal, {tag, startTime, endTime}) => {
-    const count = moment(endTime).format('x') - moment(startTime).format('x');
+    const start = moment(startTime);
+    const count = moment(endTime).format('x') - start.format('x');
+
+    if(start.format('YYYY-MM-DD') === today.format('YYYY-MM-DD'))
+      todayTotal = todayTotal + count;
 
     tagsTotal[tag] = tagsTotal[tag] + count;
     return nowTotal + count;
   }, 0);
 
   console.log(`Total: ${chalk.green(outputTime(total))}`);
+  console.log(`Today: ${chalk.green(outputTime(todayTotal))}`);
   console.log();
 
   Object.keys(tags).forEach(tag => {
